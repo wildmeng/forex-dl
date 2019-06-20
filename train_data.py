da528f2b3d35a37cf90778f53e18f70bbd21407b
meng <x.meng@genexis.eu>
2019-06-20 16:17:25 +0200

    To be continued.

--- train_data.py
+++ train_data.py
@@ -3,30 +3,30 @@ import matplotlib.pyplot as plt
 from sklearn import preprocessing
 from sklearn.utils import shuffle
 import math
+import itertools
 
 def up1(num, shift, cycles = 2):
     p = 0.5
-    x = np.linspace(0.0, 4*np.pi, num=num)
+    x = np.linspace(0.0, 2*cycles*np.pi, num=num)
     y = x*p + np.sin(x+shift)
 
     return y
 
 def up2(num, shift, cycles = 2):
-    p = 1.1
-    x = np.linspace(0.0, 4*np.pi, num=num)
+    p = 1.06
+    x = np.linspace(0.0, 2*cycles*np.pi, num=num)
     y = np.power(p,x) + np.sin(x+shift)
     return y
 
-
 def down1(num, shift, cycles = 2):
     p = -0.5
-    x = np.linspace(0.0, 4*np.pi, num=num)
+    x = np.linspace(0.0, 2*cycles*np.pi, num=num)
     y = x*p + np.sin(x+shift)
 
     return y
 
 def down2(num, shift, cycles = 2):
-    p = 1.1
+    p = 1.06
     x = np.linspace(0.0, cycles*2*np.pi, num=num)
     y = -np.power(p,x) + np.sin(x+shift)
     return y
@@ -64,6 +64,8 @@ def flat4(num, shift, cycles = 2):
     return y
 
 def add_trends(trends, period):
+    fig_num = 0
+
     x = []
     y = []
     vects = np.eye(len(trends))
@@ -73,18 +75,52 @@ def add_trends(trends, period):
                 shifts = np.linspace(0.0, np.pi, 10)
                 for shift in shifts:
                     result = ts(period, shift, c)
+                    result = preprocessing.minmax_scale(result)
                     x.append(result)
                     y.append(vects[i].tolist())
+                    # save plot
+                    x1 = np.linspace(0.0, c*2*np.pi, period)
+                    plt.plot(x1, result,'--bo')
+                    plt.savefig('./train-data/%d-%d.png'%(i, fig_num))
+                    plt.clf()
+                    fig_num += 1
 
     return x, y
 
-def get_train_data(period = 20, split_at = 0.8):
+
+def merge_trend(f1, f2, num, cycles, split_cycles):
+    n = int(num*split_cycles//cycles)
+    y1 = f1(n, 0, split_cycles)
+    y2 = f2(num-n, np.pi, cycles - split_cycles)
+
+    y2 += (y1[n-1] - y2[0])
+
+    return list(y1) + list(y2)
+
+
+def blend_trends(trends, period):
+    all_trends = list(itertools.permutations(trends, 2))
+    for t in all_trends:
+        paires = list(itertools.product(t[0], t[1]))
+        for pair in paires:
+            print(pair)
+            x1 = np.linspace(0.0, 4*2*np.pi, period)
+            y = merge_trend(pair[0], pair[1], period, 6, 4)
+            plt.plot(x1, y,'--bo')
+            plt.show()
+
+
+
+def get_train_data(period = 40, split_at = 0.8):
     trends = [
         [up1, up2],
         [down1, down2],
         [flat1,flat2,flat3,flat4]
     ]
 
+
+    blend_trends(trends, period)
+    '''
     x, y = add_trends(trends, period)
 
     assert(len(x) == len(y))
@@ -93,10 +129,11 @@ def get_train_data(period = 20, split_at = 0.8):
     x = np.array(x)
     y = np.array(y)
 
-    x = preprocessing.minmax_scale(x, axis=1)
+    #x = preprocessing.minmax_scale(x, axis=1)
     x, y = shuffle(x, y) #, random_state=0)
 
     return x, y
+    '''
 
 
 if __name__ == '__main__':
